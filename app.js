a95c3a199ed6336be3f1860b8a5db89fbcbab099
ZachHoskins <zach9414@gmail.com>
2019-09-25 06:00:56 -0500

    Removed dialogue box and finished game with buttons

--- app.js
+++ app.js
@@ -1,110 +1,122 @@
-let computerChoices = ['rock','paper','scissors'];
+let computerChoices = ['rock', 'paper', 'scissors'];
 let gameOver = false;
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection = "";
+const buttons = document.querySelectorAll('button');
 
-function computerPlay(){
-  return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+const container = document.querySelector('#text');
+const content = document.createElement('div');
+let playerScoreText = document.createElement('div');
+let computerScoreText = document.createElement('div');
+let resultsText = document.createElement('div');
+let scoreBoardText = document.createElement('div');
+const gameOverContainer = document.querySelector("#game_over_text");
+let gameOverText = document.createElement('h2');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    playerSelection = button.id;
+
+    if (!gameOver) {
+      startRound();
+    }
+  });
+});
+
+function computerPlay() {
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-function playRound(playerSelection, computerSelection){
-  if(computerSelection === 'rock'){
+function startRound() {
+  let computerSelection = computerPlay();
+  //console.log(`${playerSelection} vs ${computerSelection}`);
+  playRound(playerSelection, computerSelection);
 
-    if(playerSelection === 'paper'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log('You Win!');
+  if (playerScore === 5) {
+    gameOver = true;
+    gameOverText.style.whiteSpace = "pre"; //Allows line break in template literal
+    gameOverText.textContent = `YOU WIN!\nYOU:${playerScore} | CPU:${computerScore}`;
+  } else if (computerScore === 5) {
+    gameOver = true;
+    gameOverText.style.whiteSpace = "pre"; //Allows line break in template literal
+    gameOverText.textContent = `YOU LOSE!\nYOU:${playerScore} | CPU:${computerScore}`;
+  }
+  gameOverContainer.appendChild(gameOverText);
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerScoreText.textContent = `YOU: ${playerSelection}`;
+  computerScoreText.textContent = `CPU: ${computerSelection}`;
+
+  if (computerSelection === 'rock') {
+
+    if (playerSelection === 'paper') {
+      resultsText.textContent = 'You Win!';
       playerScore++;
       getScoreBoard();
-    } else if (playerSelection === 'scissors'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`You Lose!`);
+    } else if (playerSelection === 'scissors') {
+      resultsText.textContent = `You Lose!`;
       computerScore++;
       getScoreBoard();
-    } else if (playerSelection === computerSelection){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`It's a Tie!`);
+    } else if (playerSelection === computerSelection) {
+      resultsText.textContent = `It's a Tie!`;
       getScoreBoard();
     } else {
       alert('Please Refresh and Enter a Valid Choice');
     }
 
-  } else if (computerSelection === 'paper'){
+  } else if (computerSelection === 'paper') {
 
-    if(playerSelection === 'scissors'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log('You Win!');
+    if (playerSelection === 'scissors') {
+      resultsText.textContent = 'You Win!';
       playerScore++;
       getScoreBoard();
-    } else if (playerSelection === 'rock'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`You Lose!`);
+    } else if (playerSelection === 'rock') {
+      resultsText.textContent = `You Lose!`;
       computerScore++;
       getScoreBoard();
-    } else if (playerSelection === computerSelection){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`It's a Tie!`);
+    } else if (playerSelection === computerSelection) {
+      resultsText.textContent = `It's a Tie!`;
       getScoreBoard();
     } else {
       alert('Please Refresh and Enter a Valid Choice');
     }
 
-  } else if (computerSelection === 'scissors'){
+  } else if (computerSelection === 'scissors') {
 
-    if(playerSelection === 'rock'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log('You Win!');
+    if (playerSelection === 'rock') {
+      resultsText.textContent = 'You Win!';
       playerScore++;
       getScoreBoard();
-    } else if (playerSelection === 'paper'){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`You Lose!`);
+    } else if (playerSelection === 'paper') {
+      resultsText.textContent = `You Lose!`;
       computerScore++;
       getScoreBoard();
-    } else if (playerSelection === computerSelection){
-      console.log(`YOU: ${playerSelection}`);
-      console.log(`CPU: ${computerSelection}`);
-      console.log(`It's a Tie!`);
+    } else if (playerSelection === computerSelection) {
+      resultsText.textContent = `It's a Tie!`;
       getScoreBoard();
     } else {
       alert('Please Refresh and Enter a Valid Choice');
     }
 
-  } else{
+  } else {
     console.error("Something Went Wrong. Computer did not select Rock, Paper, or Scissors...");
   }
-}
 
-function startRound(){
-  let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-  let computerSelection = computerPlay();
-  console.log(playRound(playerSelection,computerSelection))
+  content.appendChild(playerScoreText);
+  content.appendChild(computerScoreText);
+  content.appendChild(resultsText);
+  content.appendChild(scoreBoardText);
+  container.appendChild(content);
 }
 
 // Function to display current scoreboard
-function getScoreBoard(){
-  return console.log(`YOU:${playerScore} | CPU:${computerScore}`);
+function getScoreBoard() {
+  return scoreBoardText.textContent = `YOU:${playerScore} | CPU:${computerScore}`;
 }
 
-function game(){
-  startRound();
+// function game() {
+//   startRound();
 
-  if(playerScore === 5){
-    gameOver = true;
-    alert(`YOU WIN!\n YOU:${playerScore} | CPU:${computerScore}`);
-  } else if (computerScore === 5){
-    gameOver = true;
-    alert(`YOU LOSE!\n YOU:${playerScore} | CPU:${computerScore}`);
-  } 
-}
-
-while (!gameOver){
-  game();
-}
+// }
